8fdf6f420d4080fc37f2470d2304b6df326e722f
8721son <8721son@naver.com>
2023-03-07 10:08:19 +0900

    prop passing

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import styled from 'styled-components';
+//styled-component + prop passing
+const StyledButton = styled.button`
+  color: ${(prop) => (prop.id === 2 ? 'blue' : 'red')};
+`;
 
-const Main = () => {
+const DeleteButton = styled(StyledButton)``;
+
+//스타일 상속
+const AddButton = styled(StyledButton)`
+  background-color: black;
+`;
+
+const Main = (props) => {
+  console.log(props.board);
+  // const board = props.board;
+  // const id = props.id;
+
+  //구조분할 할당 (Destructuring Assignment)
+  const { board, id, setBoard } = props;
+
   return (
     <div>
-      <h1>Main Page</h1>
+      {board.map((b) => (
+        <h1>
+          {b.title},{b.content}
+        </h1>
+      ))}
+      {id}
+      <DeleteButton id={id} onClick={() => setBoard([])}>
+        삭제
+      </DeleteButton>
+      <AddButton id={id}>추가</AddButton>
     </div>
   );
 };
